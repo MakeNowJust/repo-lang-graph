b6526a809239d6dadc97a1a077c1c9aa75705e34
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-03-07 22:21:46 +0900

    add release command

---
+++ release.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ve
+
+export LC_ALL=C
+
+cd .dest
+git init
+git remote add origin git@github.com:MakeNowJust/repo-lang-graph.git
+git checkout -b gh-pages
+git add .
+git commit -m "update `date`"
+git push --force origin gh-pages
